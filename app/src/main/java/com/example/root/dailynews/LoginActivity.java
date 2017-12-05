b746869270b84559e82326ab024ef48c6e3062a1
Harpal Singh <harpal17.singh@gmail.com>
2017-12-05 23:43:42 +0530

    Added Firebase, Login screen layout

--- app/src/main/java/com/example/root/dailynews/LoginActivity.java
+++ app/src/main/java/com/example/root/dailynews/LoginActivity.java
@@ -12,5 +12,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginscreen);
     }
 }
